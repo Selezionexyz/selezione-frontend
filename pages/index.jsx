250eb07ba7ad37574e43d954d9d296b590aba585
Selezionexyz <selezionexyzweb@gmail.com>
2025-07-06 15:35:33 +0200

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "#111",
+      color: "#fff",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "2rem"
+    }}>
+      Hello Selezione !
+    </div>
+  );
+}
